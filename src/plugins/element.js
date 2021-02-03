b531dd69c11bbc93082245518ce746257cf232d9
lichanglinshan <1119785353@qq.com>
2021-02-03 14:15:12 +0800

    user

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -20,7 +20,8 @@ import {
   Switch,
   Tooltip,
   Pagination,
-  Dialog
+  Dialog,
+  MessageBox
 } from 'element-ui'
 
 Vue.use(Button)
@@ -44,3 +45,4 @@ Vue.use(Tooltip)
 Vue.use(Pagination)
 Vue.use(Dialog)
 Vue.prototype.$message = Message
+Vue.prototype.$confirm = MessageBox.confirm
